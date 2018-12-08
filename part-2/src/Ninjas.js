68779bd8cbd494424b9c03f806832c3f56b8c013
cjewell47 <charles.jewell47@gmail.com>
2018-12-08 17:32:51 +0000

    16, conditional outputs

--- part-2/src/Ninjas.js
+++ part-2/src/Ninjas.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Ninjas = ({ninjas}) => {
 
-  const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="ninja" key={ninja.id}>
-        <div>Name: { ninja.name }</div>
-        <div>Age: { ninja.age }</div>
-        <div>Belt: { ninja.belt }</div>
-      </div>
-    )
-  });
-
   return (
     <div className="ninja-list">
-      { ninjaList }
+      {
+        ninjas.map(ninja => {
+          return ninja.age > 20 ? (
+            <div className="ninja" key={ninja.id}>
+              <div>Name: { ninja.name }</div>
+              <div>Age: { ninja.age }</div>
+              <div>Belt: { ninja.belt }</div>
+            </div>
+          ) : null
+        })
+      }
     </div>
   );
 
