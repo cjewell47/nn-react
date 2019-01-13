2b3da90b4350ddc7e8d26fdd4a1c8f7e4003146e
cjewell47 <charles.jewell47@gmail.com>
2019-01-13 13:20:40 +0000

    27, route guarding by auth status

--- full-to-do-app/src/components/projects/CreateProject.js
+++ full-to-do-app/src/components/projects/CreateProject.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { createProject } from '../../store/actions/projectActions';
+import { connect } from "react-redux";
+import { createProject } from "../../store/actions/projectActions";
+import { Redirect } from "react-router-dom";
 
 class CreateProject extends Component {
   state = {
-    title: '',
-    content: ''
+    title: "",
+    content: ""
   };
   handleChange = e => {
     this.setState({
@@ -15,9 +16,11 @@ class CreateProject extends Component {
   handleSubmit = e => {
     e.preventDefault();
     // console.log(this.state);
-    this.props.createProject(this.state)
+    this.props.createProject(this.state);
   };
   render() {
+    const { auth } = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />;
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -28,7 +31,11 @@ class CreateProject extends Component {
           </div>
           <div className="input-field">
             <label htmlFor="content"> Project Content </label>
-            <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onChange={this.handleChange}
+            />
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0"> Create </button>
@@ -39,10 +46,19 @@ class CreateProject extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapStateToProps = state => {
   return {
-    createProject: (project) => dispatch(createProject(project))
-  }
-}
+    auth: state.firebase.auth
+  };
+};
 
-export default connect(null, mapDispatchToProps)(CreateProject);
+const mapDispatchToProps = dispatch => {
+  return {
+    createProject: project => dispatch(createProject(project))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateProject);
